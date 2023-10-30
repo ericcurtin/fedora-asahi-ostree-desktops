aa665260b9bd064ad82909de3278495c2bd78f5e
Eric Curtin <ecurtin@redhat.com>
2023-10-30 20:13:32 +0000

    change

--- build-asahi-silverblue.sh
+++ build-asahi-silverblue.sh
@@ -7,5 +7,6 @@ sudo podman build .
 mkdir -p _build
 mkdir -p _build/osbuild_store/{objects,refs,sources/org.osbuild.files,tmp}
 mkdir -p _build/image_output
+osbuild-mpp silverblue-asahi-container.mpp.yaml _build/silverblue-asahi-container.json
 osbuild-mpp -I . -D image_type="ostree" -D arch="aarch64" -D distro_name="silverblue-asahi" -D target="asahi" silverblue-asahi-container.mpp.yaml _build/silverblue-asahi-container.aarch64.json
 
