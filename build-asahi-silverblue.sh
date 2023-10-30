063b3944b44977fcb9fa58a037d538dba2d742b0
Eric Curtin <ecurtin@redhat.com>
2023-10-30 18:43:08 +0000

    Add yaml

--- build-asahi-silverblue.sh
+++ build-asahi-silverblue.sh
@@ -4,9 +4,3 @@ set -ex
 
 sudo podman build .
 
-STREAM="stable"
-# as an installed binary:
-coreos-installer download -s "${STREAM}" -p qemu -f qcow2.xz --decompress -C ~/.local/share/libvirt/images/
-# or as a container:
-sudo podman run --pull=always --rm -v $HOME/.local/share/libvirt/images/:/data -w /data quay.io/coreos/coreos-installer:release download -s "${STREAM}" -p qemu -f qcow2.xz --decompress
-
