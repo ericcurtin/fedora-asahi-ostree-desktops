0aa4641fb675fb7328eecfd3799a988d941c44e0
Eric Curtin <ecurtin@redhat.com>
2023-10-30 19:31:38 +0000

    json

--- build-asahi-silverblue.sh
+++ build-asahi-silverblue.sh
@@ -4,5 +4,5 @@ set -ex
 
 sudo podman build .
 
-osbuild-mpp -I . -D image_type="ostree" -D arch="aarch64" silverblue-asahi-container.mpp.yaml
+osbuild-mpp -I . -D image_type="ostree" -D arch="aarch64" -D distro_name="silverblue-asahi" -D target="asahi" silverblue-asahi-container.mpp.yaml silverblue-asahi-container.aarch64.json
 
