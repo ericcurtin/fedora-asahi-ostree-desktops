58c6fe97c3b56eb48c36a66474e1f9c2c6682b27
Eric Curtin <ecurtin@redhat.com>
2023-11-07 11:19:58 +0000

    changes

--- build-silverblue-asahi.sh
+++ build-silverblue-asahi.sh
@@ -9,10 +9,8 @@ sudo podman build -t silverblue-asahi:39 .
 mkdir -p _build
 mkdir -p _build/osbuild_store/{objects,refs,sources/org.osbuild.files,tmp}
 mkdir -p _build/image_output
-osbuild-mpp silverblue-asahi-container.mpp.yaml _build/silverblue-asahi-container.json
+# osbuild-mpp silverblue-asahi-container.mpp.yaml _build/silverblue-asahi-container.json
 # osbuild-mpp -I . -D image_type="ostree" -D arch="aarch64" -D distro_name="silverblue-asahi" -D target="asahi" silverblue-asahi-container.mpp.yaml _build/silverblue-asahi-container.aarch64.json
 osbuild-mpp -I . silverblue-asahi-container.mpp.yaml _build/silverblue-asahi-container.aarch64.json
-sudo mkdir -p /var/osbuild/store/
-sudo osbuild --store /var/osbuild/store/ --output-directory out/ --checkpoint image-tree --checkpoint image --cache-max-size 20GiB --export qcow2 _build/silverblue-asahi-container.aarch64.json
-
+sudo osbuild --store _build/osbuild_store --output-directory _build/image_output --checkpoint image-tree --checkpoint image --cache-max-size 20GiB --export images _build/silverblue-asahi-container.aarch64.json
 
